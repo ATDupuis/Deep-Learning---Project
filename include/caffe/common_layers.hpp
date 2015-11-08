0c5c77d88da1de342111e30c63d32740c72dee5a
Allard Dupuis <atdupuis@andrew.cmu.edu>
2015-11-08 18:44:21 -0500

    Refactor weight mask initialization into separate function and function several errors

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -833,18 +833,22 @@ class TopologyLayer : public Layer<Dtype> {
  protected:
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  //virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //    const vector<Blob<Dtype>*>& top);
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  //virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+private:
+  void ConstructWeightMask();
 
   int M_;
   int K_;
   int N_;
   bool bias_term_;
   Blob<Dtype> bias_multiplier_;
+  Blob<Dtype> weight_mask_;
 };
 
 }  // namespace caffe
