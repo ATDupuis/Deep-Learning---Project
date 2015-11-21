d8409bfb2351020996cf56dc2e8662d0912deb38
Allard Dupuis <atdupuis@andrew.cmu.edu>
2015-11-21 11:19:11 -0500

    Fix compilation errors and make sure we do not try to use the CuDNN engine for convolutional topology layers yet

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -236,10 +236,10 @@ class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
 
     
 template <typename Dtype>
-class ConvolutionLayer_topology
+class ConvolutionTopologyLayer
     : public BaseConvolutionLayer<Dtype> {
     public:
-           explicit ConvolutionLayer_topology(const LayerParameter& param)
+           explicit ConvolutionTopologyLayer(const LayerParameter& param)
         : BaseConvolutionLayer<Dtype>(param) {}
         
         virtual inline const char* type() const { return "Convolution"; }
@@ -247,12 +247,12 @@ class ConvolutionLayer_topology
     protected:
         virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
                                  const vector<Blob<Dtype>*>& top);
-        virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                                 const vector<Blob<Dtype>*>& top);
+        /*virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                                 const vector<Blob<Dtype>*>& top);*/
         virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
                                   const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-        virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                                  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+        /*virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                                  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);*/
         virtual inline bool reverse_dimensions() { return false; }
         virtual void compute_output_shape();
     private:
