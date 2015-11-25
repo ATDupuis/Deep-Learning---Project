885b4fa175e99a0f0e4b53a6399636b172e73326
Allard Dupuis <atdupuis@andrew.cmu.edu>
2015-11-25 14:04:31 -0500

    Unroll failed fix

--- include/caffe/data_reader.hpp
+++ include/caffe/data_reader.hpp
@@ -65,6 +65,8 @@ class DataReader {
 
   // A source is uniquely identified by its layer name + path, in case
   // the same database is read from two different locations in the net.
+  // DD: supplemented with thread id, as this was breaking learning the same
+  // model several times in parallel with the same instance of the lib.
   static inline string source_key(const LayerParameter& param) {
     return param.name() + ":" + param.data_param().source();
   }
