855f3244cdf9703eeed08cd0e45460afa6991e73
serimp <serimp@andrew.cmu.edu>
2015-11-25 15:54:48 -0500

    Change in prototxt for mnist conv topology

---
+++ project/test_examples/mnist_lenet_topology/train_mnist_autoencoder.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+/Users/Serim/Documents/Deep-Learning---Project/build/tools/caffe train \
+  -solver=project/test_examples/mnist_lenet_topology/mnist_lenet_topology_solver.prototxt
